404bb51557ddea472f4838c060c4ab49ba8b635c
ellynhan <38302837+ellynhan@users.noreply.github.com>
2020-04-08 01:03:55 +0900

    main.js

---
+++ tutorial/main.js
@@ -0,0 +1,47 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+ 
+var app = http.createServer(function(request,response){
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    var pathname = url.parse(_url, true).pathname;
+    var title = queryData.id;
+    if(pathname == '/'){
+        fs.readFile(`data/${title}`, 'utf8', function(err, description){
+          if(title == undefined){
+          title = 'Welcome';
+          var description = 'Hello, Node.js';
+          }
+          fs.readdir('./data', function(error, filelist){
+            var list = '<ul>';
+            var i = 0;
+            while(i < filelist.length){
+              list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+              i = i + 1;
+            }
+            list = list+'</ul>';
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            ${list}
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `;
+          response.writeHead(200);
+          response.end(template);
+        })});
+    } else {
+      response.writeHead(404);
+      response.end('Not found');
+    }
+});
+app.listen(3000);
